d109acc114af1f15070a1aa694d2789f4b6cf8f5
UBER60 <rcho5318@gmail.com>
2024-08-16 14:03:24 -0700

    Update Program6.java

--- Program6.java
+++ Program6.java
@@ -9,13 +9,13 @@ public class Program6{
       System.out.println();
       double r = S.nextDouble();
       System.out.println();
-      System.out.println("The Radius of the circle = " + r);
+      System.out.println("The Radius of the circle = " + Math.round(r*1000)/1000.0);
       System.out.println();
-      System.out.println("The Diameter of the circle = " + (r*2));
+      System.out.println("The Diameter of the circle = " + Math.round(r*2*1000)/1000.0);
       System.out.println();
-      System.out.println("The Area of the circle = " + ((3.14159)*(r*r)));
+      System.out.println("The Area of the circle = " + Math.round(((3.14159)*(r*r))*1000)/1000.0);
       System.out.println();
-      System.out.println("The Circumference of the circle = " + (3.14159*r*2));
+      System.out.println("The Circumference of the circle = " + Math.round((3.14159*r*2)*1000)/1000.0);
     }
 }
 //Paste console output below:
@@ -28,8 +28,8 @@ The Radius of the circle = 3.712
 
 The Diameter of the circle = 7.424
 
-The Area of the circle = 43.28779268096
+The Area of the circle = 43.288
 
-The Circumference of the circle = 23.32316416
+The Circumference of the circle = 23.323
 
 */
